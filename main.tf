ace995f30f5842d860794a6a804e9ecf6142c454
Anup Sutar <iamanupsutar@gmail.com>
2026-03-12 14:25:46 +0530

    initialization

---
+++ main.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.medium"
+
+  tags = {
+    Name = "odoo-terraform"
+  }
+
+  user_data = file("./script.sh")
+}
---
+++ script.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+# Install Docker Engine & Docker Compose plugin
+sudo apt install git docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Start Docker & enable starting Docker at system boot
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Run Docker without sudo
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+# Deploy Odoo using Docker Compose
+git clone https://github.com/sutaranup/odoo-docker.git
+cd odoo-docker
+openssl rand -base64 16 >> odoo_pg_pass
+docker compose up -d
